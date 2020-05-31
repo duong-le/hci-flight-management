20526185aa5b3a3b5f03a697fb0350cf03b4306d
duongle26 <leduong26298@gmail.com>
2020-06-01 00:49:24 +0700

    Move navbar from top to side

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import Header from './components/header/header.component';
+import NavBar from './components/nav-bar/nav-bar.component';
 import HomePage from './pages/homepage/homepage.component';
 import FlightImagesPage from './pages/flight-images/flight-images.component';
 import FlightDataPage from './pages/flight-data/flight-data.component';
 
 import './App.css';
 
+import { Layout } from 'antd';
+const { Header, Sider, Content } = Layout;
+
 function App() {
   return (
-    <div className='App'>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/flight-data' component={FlightDataPage} />
-        <Route exact path='/flight/images' component={FlightImagesPage} />
-      </Switch>
-    </div>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider>
+        <div className="logo">HI04</div>
+        <NavBar />
+      </Sider>
+      <Layout>
+        <Header className="header-layout"></Header>
+        <Content className="content-layout">
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/flight-data" component={FlightDataPage} />
+            <Route exact path="/flight/images" component={FlightImagesPage} />
+          </Switch>
+        </Content>
+      </Layout>
+    </Layout>
   );
 }
 
