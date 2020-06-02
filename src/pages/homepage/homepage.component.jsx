01bf555567e08e2418bfe6288ab5f8c87683e0dc
NGUYEN DICH LONG <longnguyen25111998@gmail.com>
2020-06-02 09:05:52 +0700

    add: homepage component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
-const HomePage = () => <div></div>;
+import './homepage.styles.scss';
+
+const HomePage = () => (
+  <div className='home-page'>
+    <div className='background-image'></div>
+    <div className='content'>
+      <span className='title'>HI 04</span>
+      <span className='subtitle'>Flight Data Management</span>
+    </div>
+  </div>
+);
 
 export default HomePage;
