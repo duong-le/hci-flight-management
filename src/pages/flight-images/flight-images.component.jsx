772d4337658614e88821bc133f40cbf50c3658c0
NGUYEN DICH LONG <longnguyen25111998@gmail.com>
2020-06-16 06:45:37 +0700

    add: GoogleMaps API key

--- src/pages/flight-images/flight-images.component.jsx
+++ src/pages/flight-images/flight-images.component.jsx
@@ -7,7 +7,7 @@ import GoogleMapReact from 'google-map-react';
 import './flight-images.styles.scss';
 
 const Marker = () => (
-  <span className="material-icons" style={{ color: '#ea4335' }}>
+  <span className='material-icons' style={{ color: '#ea4335' }}>
     room
   </span>
 );
@@ -19,30 +19,34 @@ const FlightImagesPage = () => {
   const { flightId } = useParams();
 
   return (
-    <div className="flight-images">
-      <div className="breadcrumb">
-        <Breadcrumb separator=">">
+    <div className='flight-images'>
+      <div className='breadcrumb'>
+        <Breadcrumb separator='>'>
           <Breadcrumb.Item>
-            <Link to="/">
+            <Link to='/'>
               <HomeOutlined /> Home
             </Link>
           </Breadcrumb.Item>
           <Breadcrumb.Item>
-            <Link to="/flight-data">Flight Data Management</Link>
+            <Link to='/flight-data'>Flight Data Management</Link>
           </Breadcrumb.Item>
           <Breadcrumb.Item>{flightId}</Breadcrumb.Item>
         </Breadcrumb>
       </div>
       <Row>
         <Col xs={24} sm={24} md={24} lg={6} xl={6}>
-          <Descriptions title="Metadata" bordered column={1}>
-            <Descriptions.Item label="Author">Elianora Vasilov</Descriptions.Item>
-            <Descriptions.Item label="Drone ID">351-661-3252</Descriptions.Item>
-            <Descriptions.Item label="Filename">DSC01234.PNG</Descriptions.Item>
-            <Descriptions.Item label="File size">3.62 MB</Descriptions.Item>
-            <Descriptions.Item label="Created at">17/05/2019 18:30:41</Descriptions.Item>
-            <Descriptions.Item label="Dimension">1920 x 1080</Descriptions.Item>
-            <Descriptions.Item label="Color mode">RGB</Descriptions.Item>
+          <Descriptions title='Metadata' bordered column={1}>
+            <Descriptions.Item label='Author'>
+              Elianora Vasilov
+            </Descriptions.Item>
+            <Descriptions.Item label='Drone ID'>351-661-3252</Descriptions.Item>
+            <Descriptions.Item label='Filename'>DSC01234.PNG</Descriptions.Item>
+            <Descriptions.Item label='File size'>3.62 MB</Descriptions.Item>
+            <Descriptions.Item label='Created at'>
+              17/05/2019 18:30:41
+            </Descriptions.Item>
+            <Descriptions.Item label='Dimension'>1920 x 1080</Descriptions.Item>
+            <Descriptions.Item label='Color mode'>RGB</Descriptions.Item>
           </Descriptions>
         </Col>
 
@@ -51,9 +55,15 @@ const FlightImagesPage = () => {
         </Col>
 
         <Col xs={24} sm={24} md={24} lg={6} xl={6}>
-          <Card title="Location">
+          <Card title='Location'>
             <div style={{ height: '40vh', width: '100%' }}>
-              <GoogleMapReact bootstrapURLKeys={{ key: '' }} defaultCenter={center} defaultZoom={zoom}>
+              <GoogleMapReact
+                bootstrapURLKeys={{
+                  key: process.env.REACT_APP_GOOGLE_MAP_API_KEY
+                }}
+                defaultCenter={center}
+                defaultZoom={zoom}
+              >
                 <Marker lat={21.0051018} lng={105.8456554} />
               </GoogleMapReact>
             </div>
