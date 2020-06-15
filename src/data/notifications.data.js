2a52618acfdcd5c4e1b384e697b42bb3ab19f085
duongle26 <leduong26298@gmail.com>
2020-06-15 20:35:30 +0700

    Add notification and user menu

---
+++ src/data/notifications.data.js
@@ -0,0 +1,24 @@
+const NOTIFICATIONS = [
+  {
+    title: 'You received a new message',
+    time: '8 min',
+    icon: 'mail'
+  },
+  {
+    title: 'New user registered',
+    time: '7 hours',
+    icon: 'user-add'
+  },
+  {
+    title: 'System Alert',
+    time: '8 hours',
+    icon: 'warning'
+  },
+  {
+    title: 'You have a new update',
+    time: '2 days',
+    icon: 'sync'
+  }
+];
+
+export default NOTIFICATIONS;
